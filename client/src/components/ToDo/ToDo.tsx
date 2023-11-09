317077b9d79d64f020b32c8e362cbdef9b703eeb
mhmdhussein377 <mohammad.hussein377@gmail.com>
2023-11-09 21:40:59 +0200

    style the todo component

--- client/src/components/ToDo/ToDo.tsx
+++ client/src/components/ToDo/ToDo.tsx
@@ -1,3 +1,5 @@
+import "./index.css"
+
 import {BiSolidPencil} from "react-icons/bi"
 import {BsTrashFill} from "react-icons/bs"
 import {PiFlagPennantFill} from "react-icons/pi"
@@ -7,13 +9,13 @@ const ToDo = () => {
         <div className="todo">
             <div>
                 <div className="priority">Low</div>
-                <div><PiFlagPennantFill /></div>
+                <div><PiFlagPennantFill size={25} /></div>
             </div>
             <div>
                 <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet odio corporis inventore doloribus assumenda voluptas esse blanditiis unde molestias placeat, eaque dolore libero nisi pariatur non praesentium repellat. Ex, non?</div>
                 <div className="icons">
-                    <BiSolidPencil />
-                    <BsTrashFill />
+                    <BiSolidPencil size={25} />
+                    <BsTrashFill size={25} />
                 </div>
             </div>
         </div>
