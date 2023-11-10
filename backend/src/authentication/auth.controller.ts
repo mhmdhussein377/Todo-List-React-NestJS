dfe47b32f835d6b7dd5b5bc5a3aa6972a4ac6342
mhmdhussein377 <mohammad.hussein377@gmail.com>
2023-11-10 15:54:29 +0200

    implement user registration endpoint

--- backend/src/authentication/auth.controller.ts
+++ backend/src/authentication/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post, Req, Res } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { LoginDto } from "./dto/login-user.dto";
 import { Request, Response } from "express"
+import { RegisterUserDto } from "./dto/register-user.dto";
 
 @Controller("/auth")
 export class AuthController {
@@ -14,6 +15,23 @@ export class AuthController {
             return response.status(200).json({
                 status: "OK!",
                 message: "Successfully login!",
+                result,
+            })
+        } catch (error) {
+            return response.status(500).json({
+                status: "Error!",
+                message: "Internal Server Error!"
+            })
+        }
+    }
+
+    @Post("/register")
+    async register(@Req() request : Request, @Res() response : Response, @Body() registerDto: RegisterUserDto) : Promise<any> {
+        try {
+            const result = await this.authService.register(registerDto)
+            return response.status(200).json({
+                status: "OK!",
+                message: "Successfully registered!",
                 result,
             })
         } catch (error) {
