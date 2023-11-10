ecaa2164d7d7881ed01067d9e7c134e55f4af8f6
mhmdhussein377 <mohammad.hussein377@gmail.com>
2023-11-10 21:19:23 +0200

    create reusable CircularButton component

---
+++ client/src/components/UI/CircularButton/CircularButton.tsx
@@ -0,0 +1,11 @@
+import "./index.css"
+
+const CircularButton = ({icon}) => {
+    return (
+        <div className="circular-button">
+            {icon}
+        </div>
+    )
+}
+
+export default CircularButton
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,27 +1,24 @@
 import "./index.css"
 import { FC } from "react"
-
 import ToDos from "../../components/ToDos/ToDos"
 import {AiOutlinePlus} from "react-icons/ai"
 import {GiBackwardTime} from "react-icons/gi"
 import CreateToDo from "../../components/CreateToDo/CreateToDo"
+import CircularButton from "../../components/UI/CircularButton/CircularButton"
 
 const Home: FC = () => {
+
     return (
         <div className="home-screen">
             <div className="todos-section">
                 <ToDos/>
                 <ToDos/>
             </div>
-            <div className="buttons">
-                <div className="circular-button">
-                    <GiBackwardTime size={25} color="white"  />
-                </div>
-                <div className="circular-button">
-                    <AiOutlinePlus size={25} color="white" />
-                </div>
+            <div className="action-buttons">
+                <CircularButton icon={<GiBackwardTime size={25} color="white"/>} />
+                <CircularButton icon={<AiOutlinePlus size={25} color="white" />} />
             </div>
-            {/* <CreateToDo /> */}
+            <CreateToDo />
         </div>
     )
 }
